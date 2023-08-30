494cd62ac51738b2ee527f3558e5a118221d5a08
oserikov <srkvoa@gmail.com>
2023-08-30 12:38:03 +0300

    wip adding distinguishable usernames to bots

--- voyager_service/runner.py
+++ voyager_service/runner.py
@@ -4,7 +4,7 @@ from voyager import Voyager
 
 openai_api_key = os.getenv("MINECRAFT_OPENAI_KEY", 'your openai key')
 mc_port = os.getenv("MINECRAFT_LAN_PORT", 40000)
-mc_host = "minecraft"
+mc_host = os.getenv("MINECRAFT_LAN_HOST", "minecraft")
 machine_name = socket.gethostname()
 voyager = Voyager(
     mc_port=mc_port,
